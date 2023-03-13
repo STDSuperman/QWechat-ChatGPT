be4a51bb9ecadb0b134cebe7b3e1af2d12fadd7f
STDSuperman <2750556766@qq.com>
2023-03-13 22:49:29 +0800

    feat: modify apikey

--- apps/QWechat-ChatGPT/src/config.ts
+++ apps/QWechat-ChatGPT/src/config.ts
@@ -1,8 +1,8 @@
 import { runtimeEnv } from '@chat-common/utils/index'
 
 export const userConfig = {
-  model: 'gpt-3.5-turbo',
-  apiKey: '',
+  model: runtimeEnv.MODEL || 'gpt-3.5-turbo',
+  apiKey: runtimeEnv.OPENAI_API_KEY || '',
   chatPrivateTiggerKeyword: runtimeEnv.CHAT_PRIVATE_TRIGGER_KEYWORD || "",
   chatTiggerRule: runtimeEnv.CHAT_TRIGGER_RULE || "",
   enableGroupMessage: runtimeEnv.DISABLE_GROUP_MESSAGE === "true",
--- apps/QWechat-ChatGPT/src/types/index.ts
+++ apps/QWechat-ChatGPT/src/types/index.ts
@@ -1,6 +1,6 @@
 export interface IEnvConfig {
-  apiKey?: string;
-	model?: string;
+  OPENAI_API_KEY?: string;
+	MODEL?: string;
   CHAT_PRIVATE_TRIGGER_KEYWORD?: string;
   CHAT_TRIGGER_RULE?: string;
   DISABLE_GROUP_MESSAGE?: string;
